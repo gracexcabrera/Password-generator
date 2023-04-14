8bab6dee846e74508575b1e497fd5e663773767b
Grace Cabrera <gr4ce.cabrera@gmail.com>
2023-04-13 20:37:44 -0700

    last touches to readme and js

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specialCharacters= [
+var specialCharacters = [
   '_', '~', '[', ']', '(', ')', '{', '}', ',', '^', '#', '@', '+', '%', '?', ':', '!', '/', '//', '@', '.',
   '-', '{', '}',
 ];
@@ -11,70 +11,70 @@ var numbers = [
 ];
 
 var lowercase = [
-    'a','b','c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ]
 
 var uppercase = [
-  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
 
 function getRandomCharacter(array) {
-  var randomIndex = math.floor(math.random()= array.passwordLength);
-  var randomPasswordCharacter= array [randomIndex];
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomPasswordCharacter = array[randomIndex];
   return randomPasswordCharacter;
 }
 
 
 function generatePassword() {
 
-var passwordLength = prompt ('How many characters would you like the password to be?');
+  var passwordLength = prompt('How many characters would you like the password to be?');
 
-var hasSpecialCharacters = confirm ('Would you like special characters in your password?');
+  var hasSpecialCharacters = confirm('Would you like special characters in your password?');
 
-var hasUpperCase = confirm ('Would you like special characters in your password?');
+  var hasUpperCase = confirm('Would you like uppercase characters in your password?');
 
-var hasLowerCase = confirm ('Would you like lowercase letters?');
+  var hasLowerCase = confirm('Would you like lowercase letters?');
 
-var hasNumbers = confirm('Would you like numbers in your password?');
+  var hasNumbers = confirm('Would you like numbers in your password?');
 
-var passwordOptions = { 
-  passwordLength: passwordLength,
-  hasSpecialCharacters: hasSpecialCharacters,
-  hasLowerCase: hasLowerCase,
-  hasUpperCase: hasUpperCase,
-  hasNumbers: hasNumbers,
-}; 
+  var passwordOptions = {
+    passwordLength: passwordLength,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasLowerCase: hasLowerCase,
+    hasUpperCase: hasUpperCase,
+    hasNumbers: hasNumbers,
+  };
 
-var possiblePasswordChars= [];
-var password = [];
+  var possiblePasswordChars = [];
+  var password = [];
 
-if (passwordOptions.hasSpecialCharacters) {
-  possiblePasswordChars= possiblePasswordChars.concat (specialCharacters); 
- var randomCharacters = getRandomCharacter (specialCharacters); password.push(randomCharacters);
-}
+  if (passwordOptions.hasSpecialCharacters) {
+    possiblePasswordChars = possiblePasswordChars.concat(specialCharacters);
+  }
 
-if (passwordOptions.hasNumbers) {
-  possiblePasswordChars =possiblePasswordChars.concat(hasNumbers);
-  var randomCharacters = getRandomCharacter(hasNumbers); password.push(randomCharacters);
-}
+  if (passwordOptions.hasNumbers) {
+    possiblePasswordChars = possiblePasswordChars.concat(numbers);}
 
-if (passwordOptions.hasUpperCase) {
-  possiblePasswordChars = possiblePasswordChars.concat(hasUpperCase);
-  var randomCharacters = getRandomCharacter(hasUpperCase);password.push(randomCharacters);
-}
+  if (passwordOptions.hasUpperCase) {
+    possiblePasswordChars = possiblePasswordChars.concat(uppercase);
+  }
 
-if (passwordOptions.hasLowerCase) {
-  possiblePasswordChars = possiblePasswordChars.concat(hasLowerCase);
-  var randomCharacters = getRandomCharacter (hasLowerCase); password.push(randomCharacters);
-}
+  if (passwordOptions.hasLowerCase) {
+    possiblePasswordChars = possiblePasswordChars.concat(lowercase);
+  }
 
-for (var I = 0; i < passwordOptions.passwordLength - password.passwordLength; i++) {
-  var randomCharacters= getRandomCharacter(possiblePasswordChars); password.push(randomCharacters);
-}
+  
+  for (var i = 0; i < passwordOptions.passwordLength; i++) {
+    var randomCharacters = getRandomCharacter(possiblePasswordChars);
+     password.push(randomCharacters);
+    console.log(i)
+    console.log(password.length)
+  }
 
-
-
+  let text = password.join ("");
+  console.log ("text", text)
+  return text
 }
 
 // Write password to the #password input
@@ -88,4 +88,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
